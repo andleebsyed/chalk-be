b74bb76759c4b664b5b38061c9a6c44e4ecc99a5
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 20:11:51 +0530

    update: added a schema for User and made it's schema

---
+++ models/users-model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
+});
+
+const User = model("User", UserSchema);
+
+module.exports = { User };
