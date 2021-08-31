1bb44808fc5b67feef60cc92c9ae55442e7882f9
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 18:27:12 +0530

    updaate: added a middleware for routes which are not matched with any endpoint

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { routeNotFound } = require("./middlewares/routeNotFound");
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ status: true, message: "backend of chalk acessed successfully" });
 });
+app.use(routeNotFound);
 app.listen(9000, () => console.log("app up and running"));
