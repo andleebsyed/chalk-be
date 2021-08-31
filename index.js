675a0daac55e9f92ee0b3e270df33278e5e492af
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 20:15:11 +0530

    update: used a middleware for any request to /users endpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const { CreateDatabaseConnection } = require("./db/dbConnection");
 const { errorHandler } = require("./middlewares/errorHandler");
 const { routeNotFound } = require("./middlewares/routeNotFound");
+const { userRoute } = require("./routes/user-routes");
 require("dotenv").config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 CreateDatabaseConnection();
 app.get("/", (req, res) => {
   res.json({ status: true, message: "backend of chalk acessed successfully" });
 });
+
+app.use("/users", userRoute);
 app.use(routeNotFound);
 app.use(errorHandler);
-app.listen(9000, () => console.log("app up and running"));
+app.listen(9000 || process.env.PORT, () => console.log("app up and running"));
