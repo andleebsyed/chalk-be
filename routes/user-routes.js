c183eeeedf76d52f08f72d38288b105be7abec9b
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 10:09:02 +0530

    update: added verifyToken to /account route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { SignUp, Login, Account } = require("../controllers/users");
+const { verifyToken } = require("../middlewares/verifyToken");
 const userRoute = express.Router();
 
 userRoute.post("/login", Login);
 userRoute.post("/signup", SignUp);
-userRoute.post("/account", Account);
+userRoute.post("/account", verifyToken, Account);
 module.exports = { userRoute };
