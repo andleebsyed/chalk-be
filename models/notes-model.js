56148906cffcca3ee05d5e00b5c4ae28625c325d
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-04 13:20:25 +0530

    update: made Notes as separate modal

---
+++ models/notes-model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose();
+const NoteSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  pinned: {
+    type: Boolean,
+  },
+  color: {
+    type: String,
+  },
+  labels: [
+    {
+      type: String,
+    },
+  ],
+});
+
+const Note = model("Note", NoteSchema);
+
+module.exports = { Note };
--- models/users-model.js
+++ models/users-model.js
@@ -1,29 +1,5 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-// const NoteSchema = Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   content: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: String,
-//   },
-//   pinned: {
-//     type: Boolean,
-//   },
-//   color: {
-//     type: String,
-//   },
-//   labels: [
-//     {
-//       type: String,
-//     },
-//   ],
-// });
 const UserSchema = Schema({
   name: {
     type: String,
