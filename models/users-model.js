f56bd486559be3f1bda1ad183f2f46a985a0487f
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-04 11:04:24 +0530

    update: modified user schema so that wiring to labels and notes can be done

--- models/users-model.js
+++ models/users-model.js
@@ -1,5 +1,29 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+// const NoteSchema = Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   content: {
+//     type: String,
+//     required: true,
+//   },
+//   image: {
+//     type: String,
+//   },
+//   pinned: {
+//     type: Boolean,
+//   },
+//   color: {
+//     type: String,
+//   },
+//   labels: [
+//     {
+//       type: String,
+//     },
+//   ],
+// });
 const UserSchema = Schema({
   name: {
     type: String,
@@ -20,6 +44,7 @@ const UserSchema = Schema({
     required: true,
   },
   notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
+  labels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Label" }],
 });
 
 const User = model("User", UserSchema);
