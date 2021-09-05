e1cd751d109122dadab43404be400e822d8b465b
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 15:10:29 +0530

    update: added timestamp to notes

--- models/notes-model.js
+++ models/notes-model.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-const NoteSchema = Schema({
-  title: {
-    type: String,
-    required: true,
+const NoteSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    pinned: {
+      type: Boolean,
+    },
+    color: {
+      type: String,
+    },
+    labels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Label" }],
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  pinned: {
-    type: Boolean,
-  },
-  color: {
-    type: String,
-  },
-  labels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Label" }],
-});
+  { timestamps: true }
+);
 
 const Note = model("Note", NoteSchema);
 
