42f7c21d700343c8a52622a2638bb4ccd332115f
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 16:52:56 +0530

    update: made image upload  to cloudinary work and then saved the returned url to our note collection

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,13 @@
 const { Note } = require("../models/notes-model");
 const { User } = require("../models/users-model");
 const mongoose = require("mongoose");
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 const AddNote = async (req, res) => {
   try {
     const { userId, title, content, pinned } = req.body;
@@ -8,9 +15,24 @@ const AddNote = async (req, res) => {
     const labelsKeys = labels.map((label) =>
       mongoose.Types.ObjectId(label._id)
     );
-    const noteData = { title, content, pinned };
+    let noteData = { title, content, pinned };
     const image = req?.files?.image;
     if (image) {
+      await cloudinary.uploader.upload(
+        image.tempFilePath,
+        async (err, result) => {
+          if (err) {
+            console.log("Error occurred while uploading file");
+          } else {
+            console.log("image uplaod success");
+            const imageLink = result.secure_url;
+            noteData = {
+              ...noteData,
+              image: imageLink,
+            };
+          }
+        }
+      );
     }
     const newNote = new Note(noteData);
     let tempSavedNote = await newNote.save();
@@ -23,17 +45,17 @@ const AddNote = async (req, res) => {
     user = await User.findById(userId);
     user.notes.push(tempSavedNote._id);
     const userUpdate = await user.save();
-    res.json({
+    return res.json({
       status: true,
       message: "note added successsfully",
       newSavedNote: tempSavedNote,
       userUpdate,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       status: false,
       message: "failed to add the note",
-      errorDetail: error?.message,
+      errorDetail: error,
     });
   }
 };
