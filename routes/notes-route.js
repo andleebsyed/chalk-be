6dd0a547d5432bf3bae8ebcf64e88e0da0928080
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 19:18:50 +0530

    update: added specific routes so that action goes to desired middleware

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { AddNote } = require("../controllers/notes");
-const { verifyToken } = require("../middlewares/verifyToken");
+const { AddNote, RemovePin, PinNote } = require("../controllers/notes");
 const notesRoute = express.Router();
 notesRoute.post("/add", AddNote);
-
+notesRoute.post("/removepin", RemovePin);
+notesRoute.post("/pinnote", PinNote);
 module.exports = { notesRoute };
