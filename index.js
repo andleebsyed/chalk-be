b256e12c585b44ea93f6679b3295fd9d179f0800
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 09:19:17 +0530

    update: wired route /note to the entry point of app

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const { CreateDatabaseConnection } = require("./db/dbConnection");
 const { errorHandler } = require("./middlewares/errorHandler");
 const { routeNotFound } = require("./middlewares/routeNotFound");
 const { userRoute } = require("./routes/user-routes");
 const { labelRoute } = require("./routes/label-route");
 const { verifyToken } = require("./middlewares/verifyToken");
+const { notesRoute } = require("./routes/notes-route");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
+
 CreateDatabaseConnection();
 app.get("/", (req, res) => {
   res.json({ status: true, message: "backend of chalk acessed successfully" });
@@ -18,6 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/user", userRoute);
 app.use("/label", verifyToken, labelRoute);
+app.use("/note", verifyToken, notesRoute);
 app.use(routeNotFound);
 app.use(errorHandler);
 app.listen(9000 || process.env.PORT, () => console.log("app up and running"));
