702af1221838a2b1c99b0b731f5e124ab2876376
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 16:53:23 +0530

    update: turned this property on so that cloudinary works without any bug

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(fileUpload());
+app.use(fileUpload({ useTempFiles: true }));
 
 CreateDatabaseConnection();
 app.get("/", (req, res) => {
