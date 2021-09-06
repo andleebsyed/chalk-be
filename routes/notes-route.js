93fe8459722b279713f0e4554a04e85d729af1a5
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-07 00:13:56 +0530

    update: added route for DeleteNote controller

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -4,10 +4,12 @@ const {
   RemovePin,
   PinNote,
   UpdateNote,
+  DeletNote,
 } = require("../controllers/notes");
 const notesRoute = express.Router();
 notesRoute.post("/add", AddNote);
 notesRoute.post("/removepin", RemovePin);
 notesRoute.post("/pinnote", PinNote);
 notesRoute.post("/update", UpdateNote);
+notesRoute.post("/delete", DeletNote);
 module.exports = { notesRoute };
